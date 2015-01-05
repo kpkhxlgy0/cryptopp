7e2e4c7fb11306ebfd09106737006efd3c2d64fe
XiaoLongHan <kpkhxlgy0@163.com>
2015-01-06 02:16:18 +0800

    x

--- tools/build_win.bat
+++ tools/build_win.bat
@@ -2,8 +2,11 @@
 set DIR=%~dp0
 set DIR_TO=%DIR%\build_win32
 rd /q /s %DIR_TO%
+
 md %DIR_TO%
 cd %DIR_TO%
-cmake -G "Visual Studio 12 2013" ..\..
-cmake --build .
+cmake -G "Visual Studio 12 2013" -DCRYPTOPP_BUILD_TESTS=OFF ..\..
+cmake --build . --config Debug
+cmake --build . --config Release
+
 pause

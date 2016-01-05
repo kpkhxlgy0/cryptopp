e82aa216446cc0587eb94cdf076e545bf7447c08
Xiaolong Han <kpkhxlgy0@163.com>
2016-01-05 17:43:38 +0800

    x

--- tools/build_win.bat
+++ tools/build_win.bat
@@ -5,7 +5,7 @@ rd /q /s %DIR_TO%
 
 md %DIR_TO%
 cd %DIR_TO%
-cmake -G "Visual Studio 12 2013" -DCRYPTOPP_BUILD_TESTS=OFF ..\..
+cmake -G "Visual Studio 14 2015" -DCRYPTOPP_BUILD_TESTS=OFF ..\..
 cmake --build . --config Debug
 cmake --build . --config Release
 
